f0faace61733b70329475bc932980f84503f0ec4
CyberPy <24503627+CyberPy@users.noreply.github.com>
2024-02-14 10:43:20 -0500

    Update cc.js
    optimized CreditCard.reduce

--- src/min/cc.js
+++ src/min/cc.js
@@ -9,4 +9,4 @@ License: MIT//-
 
 import { Classic } from '/pythonmyadmin/min/cls.js';
 
-export default class CreditCard extends Classic{constructor(a){super(),this.cc=a}self_reduce(a,b){return 9<b&&(b-=9),b}self_double(a,b){0!=b%2&&(a.cc[b]=a.reduce(2*a.cc[b]))}self_iterate(a){for(var b in a.cc)a.double(b)}self_add(a){a.cc=a.cc.reduce((b,c)=>b+c,0)}self_validate(a){return a.iterate(),a.add(),0==a.cc%10}self_filter(a){for(var b in a.cc=a.cc.replace(' ',''),a.cc=a.cc.replace('-',''),a.cc=a.cc.split(''),a.cc)a.cc[b]=+a.cc[b];a.cc.reverse()}self_check(a){var b;return'string'==typeof a.cc?(a.filter(),b=a.validate()):b=!1,b}}
+export default class CreditCard extends Classic{constructor(a){super(),this.cc=a}static stc_reduce(a){return 9<a&&(a-=9),a}self_double(a,b){0!=b%2&&(a.cc[b]=a.reduce(2*a.cc[b]))}self_iterate(a){for(var b in a.cc)a.double(b)}self_add(a){a.cc=a.cc.reduce((b,c)=>b+c,0)}self_validate(a){return a.iterate(),a.add(),0==a.cc%10}self_filter(a){for(var b in a.cc=a.cc.replace(' ',''),a.cc=a.cc.replace('-',''),a.cc=a.cc.split(''),a.cc)a.cc[b]=+a.cc[b];a.cc.reverse()}self_check(a){var b;return'string'==typeof a.cc?(a.filter(),b=a.validate()):b=!1,b}}
